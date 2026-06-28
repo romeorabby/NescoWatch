3a61399d090fc5445402e598915899ebe376469e
romeorabby <133156896+romeorabby@users.noreply.github.com>
2026-06-28 20:50:44 +0600

    Update telegram

--- telegram.py
+++ telegram.py
@@ -1,7 +1,6 @@
 import os
 import requests
 
-
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
@@ -10,13 +9,20 @@ def send_message(message):
 
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+
     response = requests.post(
         url,
-        data={
-            "chat_id": CHAT_ID,
-            "text": message
-        },
+        data=payload,
         timeout=30
     )
+
+    if response.status_code != 200:
+        raise Exception(
+            f"Telegram Error : {response.text}"
+        )
 
     return response.json()
