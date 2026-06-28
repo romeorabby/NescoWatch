209308bf610962c94439222129df4f3512a8d3bf
romeorabby <133156896+romeorabby@users.noreply.github.com>
2026-06-28 20:25:49 +0600

    Rewrite tracker.py for multi consumer

--- telegram.py
+++ telegram.py
@@ -1,17 +1,22 @@
+import os
 import requests
 
-from config import BOT_TOKEN, CHAT_ID
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
 
 def send_message(message):
 
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-    data = {
-        "chat_id": CHAT_ID,
-        "text": message
-    }
+    response = requests.post(
+        url,
+        data={
+            "chat_id": CHAT_ID,
+            "text": message
+        },
+        timeout=30
+    )
 
-    response = requests.post(url, data=data)
-
-    print(response.json())
+    return response.json()
